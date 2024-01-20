4d01fa4e828deae00df2d16334d0f5fc8fd14979
Emil Rosenius Pedersen <erp@emilrosenius.com>
2024-01-20 17:36:29 +0100

    Initial commit

--- domain/blog/components/BlogPost.vue
+++ domain/blog/components/BlogPost.vue
@@ -1,7 +1,21 @@
-<script setup>
-const { name } = useLayerAComposable()
+<script setup lang="ts">
+import type { Post } from '../composables/useBlogPosts'
+
+defineProps<{
+  post: Post
+}>()
 </script>
 
 <template>
-  {{ name }}
+  <article>
+    <header>
+      <h1>{{ post.title }}</h1>
+    </header>
+    <section v-html="post.content" />
+    <footer>
+      <p>
+        <NuxtLink to="/blog">Back</NuxtLink>
+      </p>
+    </footer>
+  </article>
 </template>
--- domain/blog/pages/blog/[id].vue
+++ domain/blog/pages/blog/[id].vue
@@ -4,19 +4,9 @@ import type { Post } from '../../composables/useBlogPosts'
 const { params } = useRoute()
 const { getBlogPost } = useBlogPosts()
 
-const blogPost = ref<Post>(getBlogPost(parseInt(params.id as string)))
+const post = ref<Post>(getBlogPost(parseInt(params.id as string)))
 </script>
 
 <template>
-  <article>
-    <header>
-      <h1>{{ blogPost.title }}</h1>
-    </header>
-    <section v-html="blogPost.content" />
-    <footer>
-      <p>
-        <NuxtLink to="/blog">Back</NuxtLink>
-      </p>
-    </footer>
-  </article>
+  <BlogPost :post="post"></BlogPost>
 </template>
